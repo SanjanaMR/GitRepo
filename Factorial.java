e101f11f3013d83a089788b579189022ffab472f
SanjanaMR <sanjanamr6872@gmail.com>
2022-03-14 12:25:50 +0530

    secondprogram

---
+++ Factorial.java
@@ -0,0 +1,12 @@
+class Factorial
+{
+public static void main(String[] args)
+{
+int i;int fact=1;
+int num=5;
+for(i=1;i<=num;i++)
+{
+fact=fact*i;
+}
+System.out.println("factorial of number is:"+fact);
+}}
